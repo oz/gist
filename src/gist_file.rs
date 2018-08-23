d356208f1c90082a718be6a579b0d0c4c96334cb
Arnaud Berthomier <oz@cypr.io>
2018-08-23 13:07:30 -0500

    There is no try! cleanup :)

--- src/gist_file.rs
+++ src/gist_file.rs
@@ -7,7 +7,6 @@ use std::path::Path;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct GistFile {
     #[serde(skip_serializing, default = "GistFile::default_name")] pub name: String,
-
     #[serde(rename(serialize = "content"))] pub content: String,
 }
 
@@ -20,22 +19,19 @@ impl GistFile {
     }
 
     pub fn default_name() -> String {
-        "content".to_string()
+        String::from("content")
     }
 
     // Read standard input to content buffer.
-    pub fn read_stdin(&mut self) -> io::Result<()> {
-        try!(io::stdin().read_to_string(&mut self.content));
-        Ok(())
+    pub fn read_stdin(&mut self) -> io::Result<usize> {
+        io::stdin().read_to_string(&mut self.content)
     }
 
     // Read file to content buffer.
-    pub fn read_file(&mut self) -> io::Result<()> {
+    pub fn read_file(&mut self) -> io::Result<usize> {
         let path = Path::new(&self.name);
-        let mut fh = try!(File::open(&path));
-
-        try!(fh.read_to_string(&mut self.content));
-        Ok(())
+        let mut fh = File::open(&path)?;
+        fh.read_to_string(&mut self.content)
     }
 }
 
@@ -45,13 +41,13 @@ mod tests {
 
     #[test]
     fn read_invalid_file() {
-        let mut f = GistFile::new("/not/found.txt".to_string());
+        let mut f = GistFile::new(String::from("/not/found.txt"));
         assert!(f.read_file().is_err());
     }
 
     #[test]
     fn read_valid_file() {
-        let mut f = GistFile::new("Cargo.toml".to_string());
+        let mut f = GistFile::new(String::from("Cargo.toml"));
         assert!(f.read_file().is_ok());
     }
 }
